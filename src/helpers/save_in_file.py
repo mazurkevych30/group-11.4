e579f27cb0fda7315980b0b8106ff9c15da330e6
Oleksandr <burtovyi@gmail.com>
2025-01-08 13:29:23 +0100

    create def: ensure_data_folder, save_address_book, load_address_book, save_notes_book, load_notes_book

--- src/helpers/save_in_file.py
+++ src/helpers/save_in_file.py
@@ -1,13 +1,44 @@
+import os
 import pickle
 from src.models.address_book import AddressBook
+from src.models.notes_book import NotesBook
 
-def save_data(book, filename="addressbook.pkl"):
+# Назва папки для зберігання даних
+DATA_FOLDER = os.path.expanduser("~/.assistant_data")
+ADDRESS_BOOK_FILE = os.path.join(DATA_FOLDER, "addressbook.pkl")
+NOTES_BOOK_FILE = os.path.join(DATA_FOLDER, "notesbook.pkl")
+
+# Перевірка наявності папки для зберігання даних
+def ensure_data_folder():
+    if not os.path.exists(DATA_FOLDER):
+        os.makedirs(DATA_FOLDER)
+
+# Збереження адресної книги
+def save_address_book(book, filename=ADDRESS_BOOK_FILE):
+    ensure_data_folder()
     with open(filename, "wb") as f:
         pickle.dump(book, f)
 
-def load_data(filename="addressbook.pkl"):
+# Завантаження адресної книги
+def load_address_book(filename=ADDRESS_BOOK_FILE):
+    ensure_data_folder()
     try:
         with open(filename, "rb") as f:
             return pickle.load(f)
     except FileNotFoundError:
         return AddressBook()
+
+# Збереження нотаток
+def save_notes_book(book, filename=NOTES_BOOK_FILE):
+    ensure_data_folder()
+    with open(filename, "wb") as f:
+        pickle.dump(book, f)
+
+# Завантаження нотаток
+def load_notes_book(filename=NOTES_BOOK_FILE):
+    ensure_data_folder()
+    try:
+        with open(filename, "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return NotesBook()
